fda67bf539f8b4060b7d549550c40b9381d7dfe5
TKisely <tamaskisely@gmail.com>
2019-08-17 01:35:52 +0200

    Top down walking animations added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float _movingSpeed = 5.0f;
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    [SerializeField] private Animator _animator;
+
+    private Vector2 movement;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Input
+        movement.x=Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
         
+        _animator.SetFloat("Horizontal",movement.x);
+        _animator.SetFloat("Vertical",movement.y);
+        _animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        //Movement
+        _rigidbody2D.MovePosition(_rigidbody2D.position+Time.fixedDeltaTime*_movingSpeed*movement);
+        throw new NotImplementedException();
     }
 }
